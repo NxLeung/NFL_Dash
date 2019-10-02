70da0b516bf98529166ec2730f2b0aebc3e432e7
Nelson Leung <nelson.leung@dyn-160-39-198-182.dyn.columbia.edu>
2019-10-02 16:37:06 -0400

    boilerplate

---
+++ flask-backend/flask-backend/static/react/precache-manifest.c02951898a6ccaa0da1bd41a3905c06c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f0487ce3655d1d35a52660b2b1644a",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "d52407538d3f11a5fb7d",
+    "url": "/static/react/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ea7920225468c4d006ed",
+    "url": "/static/react/js/2.b544be00.chunk.js"
+  },
+  {
+    "revision": "d52407538d3f11a5fb7d",
+    "url": "/static/react/js/main.365bb455.chunk.js"
+  },
+  {
+    "revision": "b9008b3657f0bcc5fa86",
+    "url": "/static/react/js/runtime-main.dacdcd41.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/react/media/logo.25bf045c.svg"
+  }
+]);
